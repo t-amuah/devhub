586bd63f985bd3b22c680aed75a8b954da3af9ea
t-amuah <113646805+t-amuah@users.noreply.github.com>
2022-12-29 10:50:24 +0000

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,24 @@
+<template>
+  <div class="dashboard">
+    <UserHeader />
+    <DevAside />
+    <div class="scroll">
+      <DevMain />
+    </div>
+  </div>
+</template>
 
+<script>
+import DevAside from "./components/DevAside.vue";
+import UserHeader from "./components/UserHeader.vue";
+import DevMain from "./components/DevMain.vue";
+
+export default {
+  name: "App",
+  components: {
+    UserHeader,
+    DevAside,
+    DevMain,
+  },
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import "./assets/sass/global.scss";
+import { createApp } from "vue";
+import App from "./App.vue";
+
+createApp(App).mount("#app");
