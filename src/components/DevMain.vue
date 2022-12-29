f7caaab4d18c54d92e39c580e2ed3668ad61698a
t-amuah <113646805+t-amuah@users.noreply.github.com>
2022-12-29 10:52:44 +0000

    Add files via upload

---
+++ src/components/DevMain.vue
@@ -0,0 +1,43 @@
+<template>
+  <main class="content-wrap">
+    <header class="content-head">
+      <h1 class="is-blue">Take a test</h1>
+
+      <div class="action">
+        <button>Save search</button>
+      </div>
+    </header>
+
+    <div class="content">
+      <section class="info-boxes">
+        <InfoBox />
+        <InfoBox />
+        <InfoBox />
+        <InfoBox />
+      </section>
+
+      <section class="person-boxes">
+        <PersonBox />
+        <PersonBox />
+        <PersonBox />
+        <PersonBox />
+        <PersonBox />
+        <PersonBox />
+        <PersonBox />
+        <PersonBox />
+      </section>
+    </div>
+  </main>
+</template>
+
+<script>
+import PersonBox from "./PersonBox.vue";
+import InfoBox from "./InfoBox.vue";
+
+export default {
+  name: "DevMain",
+  components: { PersonBox, InfoBox },
+
+  props: {},
+};
+</script>
